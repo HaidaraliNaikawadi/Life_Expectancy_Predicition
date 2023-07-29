6bd4d288a9db0cc2e11d1bdb2ddad6534e36ab40
HaidaraliNaikawadi <138418238+HaidaraliNaikawadi@users.noreply.github.com>
2023-07-30 02:53:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 PORT_NUMBER=8080
 
-model_file_path=os.path.join('static','model','Decision_Tree_Regression_Life_expectancy.pkl')
+model_file_path=os.path.join('static','model','Linear_Regression_Life_expectancy.pkl')
 json_file_path=os.path.join('static','Data','json_file','json_data.json')
